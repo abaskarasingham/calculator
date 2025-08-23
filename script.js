9d088b584fc6d3667e9353096ba90322c05c8a32
Arhchu <71408816+abaskarasingham@users.noreply.github.com>
2025-08-23 19:31:56 -0400

    Add logic to the calculator to make it usable
    This will allow the calculator to make use of
    addition, subtraction, multiplication, and
    division.
    
    However, the calculator currently has some bugs
    in this state. The calculator cannot perform some
    special actions such as, chaining multiple
    operators in a single calculation, and tapping the
    '=' button to make consecutive calculations with
    the same operator and current result.
    
    I shall work towards fixing these bugs and adding
    more features in the next commit.

--- script.js
+++ script.js
@@ -15,19 +15,84 @@ function divide(a, b) {
 }
 
 function operate(operator, num1, num2) {
+    let result = 0;
+
     switch (operator) {
-        case '+': add(num1, num2);
+        case '+': result = add(num1, num2);
         break;
-        case '-': subtract(num1, num2);
+        case '-': result = subtract(num1, num2);
         break;
-        case '*': multiply(num1, num2);
+        case '*': result = multiply(num1, num2);
         break;
-        case '/': divide(num1, num2);
+        case '/': result = divide(num1, num2);
         break;
-        default: "ERROR! INVALID OPERATOR!!!";
+        default: result = "ERROR! INVALID OPERATOR!!!";
     }
+
+    return result;
 }
 
-let num1 = 0;
-let num2 = 0;
+
+
+let num1 = null;
+let num2 = null;
 let operator = '';
+
+const digitButtons = Array.from(document.querySelectorAll(".digitButton"));
+const opButtons = Array.from(document.querySelectorAll(".opButton"));
+const display = document.querySelector(".display");
+const clearButton = document.querySelector(".clearButton");
+const equalButton = document.querySelector(".equalButton");
+
+function clearBackgroundColor() {
+    opButtons.forEach(button => {
+        button.style.backgroundColor = "";
+    });
+}
+
+clearButton.addEventListener("click", function(e) {
+    display.textContent = "0";
+    num1 = null;
+    num2 = null;
+    operator = '';
+    clearBackgroundColor();
+});
+
+digitButtons.forEach(button => {
+    button.addEventListener("click", function(e) {
+        opButtons.forEach(button => {
+            if (button.style.backgroundColor) {
+                display.textContent = "0";
+                button.style.backgroundColor = "";
+            }
+        });
+
+        if (display.textContent !== "0") {
+            display.textContent += button.textContent;
+        } else {
+            display.textContent = button.textContent;
+        }
+    });
+});
+
+opButtons.forEach(button => {
+    button.addEventListener("click", function(e) {
+        if (num1 === null) {
+            num1 = +display.textContent;
+        } else {
+            num2 = +display.textContent;
+        }
+        operator = button.id;
+        clearBackgroundColor();
+        button.style.backgroundColor = "yellow";
+    });
+});
+
+equalButton.addEventListener("click", function(e) {
+    if (num1 !== null && operator !== '') {
+        num2 = +display.textContent;
+        clearBackgroundColor();
+        num1 = operate(operator, num1, num2);
+        display.textContent = num1;
+    }
+});
